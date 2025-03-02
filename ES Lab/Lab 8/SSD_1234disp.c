dd469e561169a124da4b74d7ac08d9af5c3ca0a0
D-Coder-42 <137105924+D-Coder-42@users.noreply.github.com>
2025-03-02 07:17:03 +0530

    Added basic 7-segment Display Programs

---
+++ ES Lab/Lab 8/SSD_1234disp.c
@@ -0,0 +1,26 @@
+#include <lpc17xx.h>
+
+unsigned int digits[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+unsigned int i,j;
+
+int main () {
+	SystemInit();
+	SystemCoreClockUpdate();
+	
+	LPC_PINCON->PINSEL0 &= ~(0xFFFF << 8);
+	LPC_PINCON->PINSEL3 &= ~(0xFF << 14);
+	
+	LPC_GPIO0->FIODIR |= (0xFF << 4);
+	LPC_GPIO1->FIODIR |= (0xF << 23);
+	
+	while (1) {
+		for (i = 0; i < 4; i++) {
+			LPC_GPIO1->FIOPIN &= ~(0xF << 23);
+			LPC_GPIO1->FIOPIN |= (i << 23);
+			LPC_GPIO0->FIOPIN &= ~(0xFF << 4);
+			LPC_GPIO0->FIOPIN |= digits[3-i] << 4;
+			
+			for (j = 0; j<10000; j++);
+		}
+	}
+}
---
+++ ES Lab/Lab 8/SSD_HEXupdowncounter.c
@@ -0,0 +1,50 @@
+#include <lpc17xx.h>
+
+unsigned int digits[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+unsigned int i,j,count,sw_pin;
+
+void displaySSD(int);
+
+int main () {
+	SystemInit();
+	SystemCoreClockUpdate();
+	
+	LPC_PINCON->PINSEL0 &= ~(0xFFFF << 8);
+	LPC_PINCON->PINSEL3 &= ~(0xFF << 14);
+	LPC_PINCON->PINSEL1 &= ~(0x03<<10);
+	
+	LPC_GPIO0->FIODIR |= (0xFF << 4);
+	LPC_GPIO1->FIODIR |= (0xF << 23);
+	LPC_GPIO0->FIODIR &= ~(0x1<<21);
+	
+	count = 0;
+	
+	while (1) {
+		sw_pin = (LPC_GPIO0->FIOPIN & (1 << 21)) >> 21;
+		count += sw_pin == 1? 1 : -1;
+		
+		displaySSD(count);
+		
+		if (count == (1 << 16) - 1 && sw_pin == 1) count = 0;
+		if (count == 0 && sw_pin == 0) count = (1 << 16) - 1;
+		
+		for (j = 0; j<40000; j++);
+	}
+}
+
+void displaySSD (int num) {
+	int d[4] = {0};
+	d[3] = num % 16;
+	d[2] = num/16 % 16;
+	d[1] = num/(16 << 4) % 16;
+	d[0] = num/(16 << 8) % 16;
+	
+	for (i = 0; i < 4; i++) {
+		LPC_GPIO1->FIOPIN &= ~(0xF << 23);
+		LPC_GPIO1->FIOPIN |= (i << 23);
+		LPC_GPIO0->FIOPIN &= ~(0xFF << 4);
+		LPC_GPIO0->FIOPIN |= digits[d[3-i]] << 4;
+			
+		for (j = 0; j<10000; j++);
+	}
+}
